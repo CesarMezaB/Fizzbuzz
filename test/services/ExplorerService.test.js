52011649037ed0b0a05cde79b40157b8e8d66f4b
cesarmezab <99105052+CesarMezaB@users.noreply.github.com>
2022-04-29 12:04:08 -0700

    explorers test on a mission

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -2,16 +2,16 @@ const ExplorerService = require("../../lib/services/ExplorerService");
 const Reader = require("./../../lib/utils/Reader");
 
 describe("Test para ExplorerService", () =>{ 
-  const explorers= Reader.readJsonFile("./explorers.json")
-  test('Requerimiento 1: Calcular todos los explorer en una mision', () => {
-      //const explorers = [{mission : "node"}];
-      const explorersInNode = ExplorerService.filterByMission(explorers, "node")
-      expect(explorersInNode.length).toBe(10);
-})
+    const explorers= Reader.readJsonFile("./explorers.json");
+    test("Requerimiento 1: Calcular todos los explorer en una mision", () => {
+        //const explorers = [{mission : "node"}];
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+        expect(explorersInNode.length).toBe(10);
+    });
 
-test('Requerimiento 1: Calcular todos los explorer en una mision', () => {
-  //const explorers = [{mission : "node"}];
-  const explorersInNode = ExplorerService.filterByMission(explorers, "node")
-  expect(explorersInNode.length).toBe(10);
-})
+    test("Requerimiento 1: Calcular todos los explorer en una mision", () => {
+        //const explorers = [{mission : "node"}];
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+        expect(explorersInNode.length).toBe(10);
+    });
 });
