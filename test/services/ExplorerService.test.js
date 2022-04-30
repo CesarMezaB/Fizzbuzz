63b8bfc23c5eef333b589efe9b182253926976bc
cesarmezab <99105052+CesarMezaB@users.noreply.github.com>
2022-04-29 18:36:59 -0700

    Get the number of explorers that are in node method

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,17 +1,22 @@
 const ExplorerService = require("../../lib/services/ExplorerService");
 const Reader = require("./../../lib/utils/Reader");
 
+
 describe("Test para ExplorerService", () =>{ 
     const explorers= Reader.readJsonFile("./explorers.json");
+    
     test("Requerimiento 1: Calcular todos los explorer en una mision", () => {
         //const explorers = [{mission : "node"}];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(10);
+        expect(explorersInNode[0]).toMatchObject({ mission: "node"});
     });
 
-    test("Requerimiento 1: Calcular todos los explorer en una mision", () => {
-        //const explorers = [{mission : "node"}];
-        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
-        expect(explorersInNode.length).toBe(10);
+    test("Requerimiento 2: cantidad de explorers que están en node", () => {
+        const explorers= Reader.readJsonFile("./explorers.json");
+        const explorersInMission = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+        console.log(explorersInMission);
+        expect(explorersInMission).toBe(10);
     });
+    
 });
