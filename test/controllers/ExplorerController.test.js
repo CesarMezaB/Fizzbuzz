0adc5cb99676f22493f07ddcdba750aafe56003d
cesarmezab <99105052+CesarMezaB@users.noreply.github.com>
2022-05-01 20:20:33 -0700

    Add test:  Returning the list of usernames of the explorers filtered by the mission sent

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -2,10 +2,14 @@ const ExplorerController = require("../../lib/controllers/ExplorerController")
 describe("Test para ExplorerService", () =>{ 
    
     
-    test("Requerimiento 1: Calcular todos los explorer en una mision", () => {
-        //const explorers = [{mission : "node"}];
+    test("Requerimiento 1: Regresando lista de explorers", () => {
         const explorerByMission = ExplorerController.getExplorersByMission("node");
         expect(explorerByMission[0].name).toBe("Woopa1");  
+    });
+
+    test("Requerimiento 2: Regresa la lista de usernames de los explorers filtrados por la misión enviada", () => {
+        const explorerByMission = ExplorerController.getExplorersByMission("node");
+        expect(explorerByMission[1].githubUsername).toBe("ajolonauta2");  
     });
     
 });
